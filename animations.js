00de2babbf3962145d35feb522009a98b98213d5
kewinczi <213432@student.pwr.edu.pl>
2018-12-06 21:05:14 +0100

    improve animation

--- animations.js
+++ animations.js
@@ -6,8 +6,3 @@ function animateContent() {
 var links = document.getElementsByClassName("nav-link");
 console.log(links);
 Object.keys(links).map(link => links[link].addEventListener("click", animateContent));
-
-window.onload = function() {
-    document.getElementById("main").classList.add("title");
-    console.log("onload");
-}
