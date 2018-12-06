994ef0c9f00fd385d509685591106f3c844d05c1
kewinczi <213432@student.pwr.edu.pl>
2018-12-06 20:59:30 +0100

    change background image, add animations

---
+++ animations.js
@@ -0,0 +1,13 @@
+function animateContent() {
+    document.getElementById("main").classList.remove("animate");
+    setTimeout(function(){ document.getElementById("main").classList.add("animate"); }, 0);
+}
+
+var links = document.getElementsByClassName("nav-link");
+console.log(links);
+Object.keys(links).map(link => links[link].addEventListener("click", animateContent));
+
+window.onload = function() {
+    document.getElementById("main").classList.add("title");
+    console.log("onload");
+}
