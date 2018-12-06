593f566d6da89a25075b3950bfef80d9400d0725
kewinczi <213432@student.pwr.edu.pl>
2018-12-06 21:16:45 +0100

    animation again

--- animations.js
+++ animations.js
@@ -6,3 +6,9 @@ function animateContent() {
 var links = document.getElementsByClassName("nav-link");
 console.log(links);
 Object.keys(links).map(link => links[link].addEventListener("click", animateContent));
+
+window.onload = function() {
+    document.getElementById("main").classList.add("animate-init");
+    setTimeout(function(){ document.getElementById("main").classList.remove("animate-init"); }, 1000);
+    console.log("onload");
+}
